81600ca10cc5388225275b60bde0efda7e656529
Gabrielle La Madrid <galamadrid917@gmail.com>
2021-10-18 21:17:58 -0700

    ClientonCode

--- db/client.js
+++ db/client.js
@@ -1,2 +1,14 @@
 // build and export your unconnected client here
 
+const CONNECTION_STRING = process.env.DATABASE_URL || "postgres://localhost:5432/phenomena-dev";
+
+// Create the client using new Client(CONNECTION_STRING)
+const client = new Client({
+  connectionString: CONNECTION_STRING,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = {  
+    client
+  }
+  
--- db/index.js
+++ db/index.js
@@ -1,1 +1,8 @@
 // require and re-export all files in this db directory (users, activities...)
+const { client } = require('pg'); // imports the pg module
+const express = require('express');
+
+module.exports = {  
+    client
+  }
+  
